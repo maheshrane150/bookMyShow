60fcf91b39b7f209d8886aeb3f4a94aaa05bd621
Hemant610 <82433701+Hemant610@users.noreply.github.com>
2023-06-19 18:14:16 +0530

    first commit
    first commit

---
+++ db/connection.js
@@ -0,0 +1,44 @@
+const { MongoClient } = require("mongodb");
+//let mongoose = require("mongoose");
+//require("dotenv").config();
+
+const mongoose=require("mongoose");
+const dbUrl ="mongodb+srv://hemant:hemant@cluster0.pdk4bmf.mongodb.net/?retryWrites=true&w=majority"
+const connectionParams ={
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+}
+const connectToMongo = async () => {
+mongoose.connect(dbUrl, connectionParams).then(()=>{
+  console.info("Connection on the DB")
+}).catch((e)=>{
+  console.log("Error:", e);
+});
+}
+
+
+// for production use this 
+//const mongoLiveURI = process.env.MONGO_LIVE_URL;
+
+// for development use this
+//const mongoLocalURI = process.env.MONGO_URL;
+// const mongoURI = "mongodb://localhost:27017/bookMovie" + "bookMovie"
+
+//const connectToMongo = async () => {
+  // Connecting to database using connection string and speciying if there is any error or it was successfull
+  // mongoose
+  //   .connect( mongoLiveURI, 
+  //       { 
+  //           useNewUrlParser: true, 
+  //           useUnifiedTopology: true 
+  //       }
+  //   )
+  //   .then(() => {
+  //     console.log("Database connected");
+  //   })
+  //   .catch((err) => {
+  //     console.log("Database connection error", err);
+  //   });
+//};
+
+exports.connection = connectToMongo;
