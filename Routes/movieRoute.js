e60e594c26b87d686a9dbaea5769e2b68e7a129b
Hemant610 <82433701+Hemant610@users.noreply.github.com>
2023-06-19 18:18:33 +0530

    de
    de

--- Routes/movieRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const controller = require("../Controller/movieController")
-
-router.post("/booking", controller.storeBooking)
-router.get("/booking", controller.getBooking)
-
-module.exports = router
