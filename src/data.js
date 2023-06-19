d20f0b227e444fd85c167e2855ac39707934c322
Hemant610 <82433701+Hemant610@users.noreply.github.com>
2023-06-19 21:04:57 +0530

    de
    de

--- src/data.js
+++
@@ -1,7 +0,0 @@
-export let moviesList = ['Adipurush', 'The Flash', 'The Kerala Story', 'Avengers End Game', 'The Hulk', "X-Man", "SuperMan"];
-export let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM", "11:30 PM"];
-export let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
-
-// exports.moviesList = moviesList;
-// exports.slots = slots;
-// exports.seats = seats;
