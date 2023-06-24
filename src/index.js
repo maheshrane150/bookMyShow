e73a9f16cf0319d8e2d70aed6ae21f88fa0b1785
Hemant610 <82433701+Hemant610@users.noreply.github.com>
2023-06-24 20:18:37 +0530

    Update index.js
    commit first

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from "react"; // import the react
+import ReactDOM from "react-dom/client"; //import the react-dom client and then import style index css
 import "./index.css";
 import App from "./App";
 
