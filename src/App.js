b633db763316b551dac4f13e9594d9c2fc620d70
Hemant610 <82433701+Hemant610@users.noreply.github.com>
2023-06-24 20:21:24 +0530

    Update App.js
    commit first

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React from "react";// import the react and app style and BsState and pages import and -->
 import "./App.css";
 import BsState from "./Context/BsState";
 import Home from "./Pages/Home";
-
+//return it------------------------------------------------------------------------------------>
 function App() {
   return (
     <>
