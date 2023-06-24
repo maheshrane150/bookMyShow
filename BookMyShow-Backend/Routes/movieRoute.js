cbec7ed2f7d58c10f7da767a4928079124e1dd1e
Hemant610 <82433701+Hemant610@users.noreply.github.com>
2023-06-24 20:01:27 +0530

    Update movieRoute.js
    commit first

--- BookMyShow-Backend/Routes/movieRoute.js
+++ BookMyShow-Backend/Routes/movieRoute.js
@@ -1,7 +1,9 @@
+//created the express and router & controller--------->
 const express = require('express')
 const router = express.Router()
 const controller = require("../Controller/movieController")
 
+//post and get booking are storeBooking and GetBooking 
 router.post("/booking", controller.storeBooking)
 router.get("/booking", controller.getBooking)
 
