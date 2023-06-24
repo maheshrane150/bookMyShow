a69c6390a3e6eeb7fcc1d61ee4c80a3ee59e3206
Hemant610 <82433701+Hemant610@users.noreply.github.com>
2023-06-24 19:58:54 +0530

    Update connection.js
    commit first

--- BookMyShow-Backend/db/connection.js
+++ BookMyShow-Backend/db/connection.js
@@ -1,13 +1,15 @@
+//connect the MongoClient require mongodb -------------->
 const { MongoClient } = require("mongodb");
-//let mongoose = require("mongoose");
-require("dotenv").config();
+
 
 const mongoose=require("mongoose");
-const dbUrl = process.env.MONGODB_URL
+
+const dbUrl = process.env.MONGODB_URL//Please Copy and paste the address of Majority
 const connectionParams ={
   useNewUrlParser: true, 
   useUnifiedTopology: true 
 }
+// for development use this
 const connectToMongo = async () => {
 mongoose.connect(dbUrl, connectionParams).then(()=>{
   console.info("Connection on the DB")
@@ -15,30 +17,5 @@ mongoose.connect(dbUrl, connectionParams).then(()=>{
   console.log("Error:", e);
 });
 }
-
-
-// for production use this 
-//const mongoLiveURI = process.env.MONGO_LIVE_URL;
-
-// for development use this
-//const mongoLocalURI = process.env.MONGO_URL;
-// const mongoURI = "mongodb://localhost:27017/bookMovie" + "bookMovie"
-
-//const connectToMongo = async () => {
-  // Connecting to database using connection string and speciying if there is any error or it was successfull
-  // mongoose
-  //   .connect( mongoLiveURI, 
-  //       { 
-  //           useNewUrlParser: true, 
-  //           useUnifiedTopology: true 
-  //       }
-  //   )
-  //   .then(() => {
-  //     console.log("Database connected");
-  //   })
-  //   .catch((err) => {
-  //     console.log("Database connection error", err);
-  //   });
-//};
 
 exports.connection = connectToMongo;
